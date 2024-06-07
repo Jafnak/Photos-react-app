1d5d30966248638ff3f13f09db6f4bd743b4af46
Jafnak <jafnakusman@gmail.com>
2024-06-08 01:21:56 +0530

    photo-app  components added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import Add from './components/Add';
+import Search from './components/Search';
+import Delete from './components/Delete';
+import ViewAll from './components/ViewAll';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div>
-      <Add/>
-    </div>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Add/>}/>
+        <Route path='/search' element={<Search/>}/>
+        <Route path='/delete' element={<Delete/>}/>
+        <Route path='/viewall' element={<ViewAll/>}/>
+        
+      </Routes>
+    </BrowserRouter>
+    
+
   );
 }
 
